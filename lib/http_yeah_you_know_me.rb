b188f1465cfade7e8322234caf5d669d16b6e2a2
PenneyGadget <dreamofgreen@gmail.com>
2015-11-02 14:55:26 -0700

    iteration 0 implemented - Hello, World

--- lib/http_yeah_you_know_me.rb
+++ lib/http_yeah_you_know_me.rb
@@ -7,16 +7,26 @@ class HttpYeahYouKnowMe
   #   @server = TCPServer.new(port)
   #   @client = @server.accept
   # end
+
   tcp_server = TCPServer.new(9292)
-  client = tcp_server.accept
+  request_counter = 0
 
-  # def response
-  #   "Hello, World"
-  # end
-  response = "Hello, World"
-  # output = "<html><head></head><body>#{response}</body></html>"
+  loop do
+    client = tcp_server.accept
 
-  client.puts response
+    response = "<pre>" + ("Hello, World (#{request_counter})\n") + "</pre>"
+    output = "<html><head></head><body>#{response}</body></html>"
+    headers = ["http/1.1 200 ok",
+               "date: #{Time.now.strftime('%a, %e %b %Y %H %M %S %z')}",
+               "server: ruby",
+               "content-type: text/html; charset=iso-8859-1",
+               "content-length: #{output.length}\r\n\r\n"].join("\r\n")
+
+    request_counter += 1
+    client.puts headers
+    client.puts output
+  end
+
   client.close
 
 end
