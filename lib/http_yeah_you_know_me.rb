ad7f2c1af8394e38ddf7a491569a72d228bc45ad
PenneyGadget <dreamofgreen@gmail.com>
2015-11-02 17:48:07 -0700

    setup for second iteration

--- lib/http_yeah_you_know_me.rb
+++ lib/http_yeah_you_know_me.rb
@@ -3,30 +3,53 @@ require 'socket'
 
 class HttpYeahYouKnowMe
 
-  # def initialize(port)
-  #   @server = TCPServer.new(port)
-  #   @client = @server.accept
-  # end
+  def initialize(port)
+    @server = TCPServer.new(port)
+  end
 
-  tcp_server = TCPServer.new(9292)
-  request_counter = 0
+  def run
+    request_counter = 0
+    loop do
+      client = @server.accept
+      request_lines = parse_request(client)
+      request_counter += 1
+      output = build_response_body(request_counter, request_lines)
+      headers = build_response_headers(output)
+      return_response(client, headers, output)
+    end
+    client.close
+  end
 
-  loop do
-    client = tcp_server.accept
+  def parse_request(client)
+    request_lines = []
+    while line = client.gets and !line.chomp.empty?
+      request_lines << line.chomp
+    end
+    request_lines
+  end
 
-    response = "<pre>" + ("Hello, World! (#{request_counter})\n") + "</pre>"
+  def build_response_body(request_counter, request_lines)
+    response = "<pre>" + ("Hello, World! (#{request_counter})\n\n#{request_lines}") + "</pre>"
     output = "<html><head></head><body>#{response}</body></html>"
+  end
+
+  def build_response_headers(output)
     headers = ["http/1.1 200 ok",
                "date: #{Time.now.strftime('%a, %e %b %Y %H %M %S %z')}",
                "server: ruby",
                "content-type: text/html; charset=iso-8859-1",
                "content-length: #{output.length}\r\n\r\n"].join("\r\n")
+  end
 
-    request_counter += 1
+  def return_response(client, headers, output)
     client.puts headers
     client.puts output
   end
 
-  client.close
+end
 
+
+if __FILE__ == $0
+  server = HttpYeahYouKnowMe.new(9292)
+  server.run
 end
