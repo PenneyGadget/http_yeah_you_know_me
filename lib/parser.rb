8304030aff689f0a18421dd2bb6fea50ce7abf54
PenneyGadget <dreamofgreen@gmail.com>
2015-11-09 08:20:06 -0700

    Tests for diagnostics

--- lib/parser.rb
+++ lib/parser.rb
@@ -5,16 +5,16 @@ class Parser
     counter = 0
     info = Hash.new
     while line = client.gets and !line.chomp.empty?
-        if counter == 0
-          info["Verb"] = line.split(" ").first
-          info["Path"] = line.split(" ")[1]
-          info["Protocol"] = line.split(" ")[2]
-        else
-          keyval = line.scan(/([^:]*):(.*$)/)
-          info[keyval[0][0]] = keyval[0][1]
-        end
-        counter += 1
+      if counter == 0
+        info["Verb"] = line.split(" ").first
+        info["Path"] = line.split(" ")[1]
+        info["Protocol"] = line.split(" ")[2]
+      else
+        keyval = line.scan(/([^:]*):(.*$)/)
+        info[keyval[0][0]] = keyval[0][1]
       end
+      counter += 1
+    end
     info
   end
 
